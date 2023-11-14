cf65de9c997999ac2eb247a33ba6ee0913a7126a
joaopaulozx <132359753+joaopaulozx@users.noreply.github.com>
2023-11-14 14:48:46 -0300

    compras

---
+++ src/app/sessaocompras/sessaocompras.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-sessaocompras',
+  templateUrl: './sessaocompras.component.html',
+  styleUrls: ['./sessaocompras.component.scss']
+})
+export class SessaocomprasComponent {
+  comprar(){
+    Swal.fire({
+      title: "Você desja comprar esse produto?",
+      showDenyButton: true,
+      icon: "question",
+      confirmButtonText: "Sim",
+      denyButtonText: `Cancelar`
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  }
+}
